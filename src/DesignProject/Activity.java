e1d27e5e3e1ce1ffae4130c6f4b65a73c1950f15
Kiwilynn <kasll@hotmail.com>
2019-09-19 11:45:41 +0200

    variables added in Activity class ( K&N)

--- src/DesignProject/Activity.java
+++ src/DesignProject/Activity.java
@@ -2,10 +2,46 @@ package DesignProject;
 
 public class Activity {
     private String name;
+    private String dateTime;
+    private String equipment;
+    private int ageRequirement;
+    private int heightRequirement;
     private Object Employee; //the  Employee object is used variable so its usable in constructor
 
     public String getName() {
         return name;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public int getAgeRequirement() {
+        return ageRequirement;
+    }
+
+    public void setAgeRequirement(int ageRequirement) {
+        this.ageRequirement = ageRequirement;
+    }
+
+    public int getHeightRequirement() {
+        return heightRequirement;
+    }
+
+    public void setHeightRequirement(int heightRequirement) {
+        this.heightRequirement = heightRequirement;
     }
 
     public void setName(String name) {
